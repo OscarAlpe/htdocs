cf0489d5f13686cf9e09e0af57eb1a311c97b913
Oscar Megía López <pfgytk@gmail.com>
2019-11-21 14:04:48 +0100

    Terminada practica ahorcado en academia

--- m3/u1/practicas/20191120/ahorcado/funciones.php
+++ m3/u1/practicas/20191120/ahorcado/funciones.php
@@ -1,14 +1,11 @@
 <?php
     function existe_caracter($cadena, $caracteres) {
         $salida = [];
-        $index = 0;
         
         for ($i=0; $i<strlen($cadena); $i++) {
             for ($j=0; $j<strlen($caracteres); $j++) {
                 if ($cadena[$i] == $caracteres[$j]) {
-                    $salida[$index]["posicion"] = $i;
-                    $salida[$index]["letra"] = $caracteres[$j];
-                    $index++;
+                    $salida[] = $i;
                     break;
                 }
             }
@@ -19,9 +16,9 @@
 
     function crea_acertados($cadena, $array_acertados) {
         $salida = str_repeat("_", strlen($cadena));
-        for ($i=0; $i<strlen($cadena); $i++) {
+        for ($i=0; $i < strlen($cadena); $i++) {
             foreach ($array_acertados as $value) {
-                $salida[$value["posicion"]] = $value["letra"];
+                $salida[$value] = $cadena[$value];
             }
         }
         
@@ -41,6 +38,9 @@
         return $salida;
     }
     
+    function generateRandomString($length = 10) { 
+        return substr(str_shuffle("abcdefghijklmnopqrstuvwxyz "), 0, $length);
+    } 
     /*
     
     $array_encontrados = existe_caracter("esto es una prueba", "e");
